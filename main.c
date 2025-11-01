61714c89db1781df4792d809be4d70ccadd71098
r0me777 <58448938+r0me777@users.noreply.github.com>
2025-11-01 13:35:19 -0500

    inital main file

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int NumberofRounds = 10; //Typical rounds for 128  bit AES encryption | From Slides
+
+int input = ""
+
+//This will be made in ECB Mode, as it will encrypt and decrypt one plaintext at a time without an additive
+
+ShiftRow () {
+
+}
+
+MixColumn () {
+
+}
+
+AddRoundKey () {
+
+}
+
+ 
+ShiftBytes () {
+    
+}
+
+Struct AES (int input, int NumberofRounds, key?) { //Used for both encryption and decryption since everything is inveritable. 
+    
+
+}
+
+
+int main() {
+        
+    printf("AES, program");
+    return 0;
+}
